cf33360f65927c6b7742e33402dcf3108db20cf9
abdiqafar <iabdiqafar24@gmail.com>
2024-10-05 13:23:55 +0300

    complete mini project

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('create-task-form');
-  const taskList = document.getElementById('tasks');
-
-  form.addEventListener('submit', (event) => {
-      event.preventDefault(); // Prevent the default form submission
-
-      const taskDescription = document.getElementById('new-task-description').value;
-      const taskPriority = document.getElementById('task-priority').value;
-
-      if (taskDescription) {
-          addTask(taskDescription, taskPriority);
-          form.reset(); // Clear the form input
-      }
-    });
+    const form = document.getElementById('create-task-form');
+    const task = document.getElementById('tasks');
   
-  function addTask(description, priority) {
-      const taskItem = document.createElement('li');
-      taskItem.textContent = description;
-      taskItem.className = priority; // Add priority as a class for styling
-
-      const deleteButton = document.createElement('button');
-      deleteButton.textContent = 'Delete';
-      deleteButton.addEventListener('click', () => {
-          taskItem.remove(); // Remove the task from the list
+    form.addEventListener('submit', (event) => {
+              event.preventDefault(); 
+              const taskDescription = document.getElementById('new-task-description').value;
+              const taskPriority = document.getElementById('task-priority').value;
+              if (taskDescription) {
+                addTask(taskDescription, taskPriority);
+                form.reset(); 
+        }
       });
     
-      taskItem.appendChild(deleteButton);
-      taskList.appendChild(taskItem);
-  }
-});
-
-
+    function addTask(description, priority) {
+        const taskItem = document.createElement('li');
+        taskItem.textContent = description;
+        taskItem.className = priority;
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => {
+            taskItem.remove(); 
+        });
+      
+        taskItem.appendChild(deleteButton);
+        task.appendChild(taskItem);
+    }
+  });
