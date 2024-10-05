3621f166843b6b07fdf4ef0c970f1066955f69b5
abdiqafar <iabdiqafar24@gmail.com>
2024-10-05 14:08:28 +0300

    complete mini project

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('create-task-form');
     const task = document.getElementById('tasks');
   
-    form.addEventListener('submit', (e) => {
-              e.preventDefault(); 
+    form.addEventListener('submit', (event) => {
+              event.preventDefault(); 
               const taskDescription = document.getElementById('new-task-description').value;
               const taskPriority = document.getElementById('task-priority').value;
               if (taskDescription) {
